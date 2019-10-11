ca2e760a5aaf8377fd6d4f9309f4579bba3620ff
Francisco Ayala Le Brun <frankxlebrun@gmail.com>
2019-10-11 16:50:50 +0200

    Add constants file

---
+++ src/game/game_constants.asm
@@ -0,0 +1,9 @@
+%define bomber_initial_y 11
+%define bomber_initial_x 320/2 - 32
+%define bomber_initial_speed 0xF
+%define bomber_initial_state 1	
+
+	
+%define paddle_initial_y 200-30
+%define paddle_initial_x 320/2 - 16
+%define paddle_speed 0x25
